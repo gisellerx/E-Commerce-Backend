947a1aaca5d0cc63e3ab14707a0bef4eefa97ee9
gisellerx <giselle.armas@outlook.com>
2023-10-12 21:04:03 -0400

    attempting to resolve internal node issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('../config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +9,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//http://localhost:3001 forumulating in your middleware app.use
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize
+.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+})
